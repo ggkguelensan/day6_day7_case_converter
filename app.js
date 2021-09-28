bd6f8507fb28ed5b0d131d9b46968326e8371cbc
ggkguelensan <gkguelensan@gmail.com>
2021-09-28 23:46:57 +0300

    js add first_char function

--- app.js
+++ app.js
@@ -15,14 +15,20 @@ proper_case_button.addEventListener("click", () => {
     textarea_node.value = to_proper_string(textarea_node.value)
 });
 sentence_case_button.addEventListener("click", () => {
-    textarea_node.value = textarea_node.value.toLowerCase().match(/[^.!?]+[\s.!?]*/g).map(sentence =>
-        sentence[0].toUpperCase() + sentence.slice(1)
-    ).join("");
+    textarea_node.value = to_sentence_string(textarea_node.value);
 });
 
 function to_proper_string(text) {
     let array_of_words = text.toLowerCase().match(/[^\s.!?]+[\s.!?]*/g);
-    return array_of_words.map(word =>
-        word[0].toUpperCase() + word.slice(1)
-    ).join("");
+    log(array_of_words)
+    return array_of_words.map(word => first_char_to_upper(word)).join("");
+}
+
+function to_sentence_string(text) {
+    array_of_sentences = text.toLowerCase().match(/[^.!?]+[\s.!?]*/g);
+    return array_of_sentences.map(sentence => first_char_to_upper(sentence)).join("");
+}
+
+function first_char_to_upper(string) {
+    return string[0].toUpperCase() + string.slice(1)
 }
